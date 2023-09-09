31b1b96beebce58af3afccfb89faabbc9317ccbc
Hamada Aljarrah <hamadasepost@gmail.com>
2023-09-10 00:54:25 +0200

    change server url

--- src/model/socket.ts
+++ src/model/socket.ts
@@ -1,4 +1,4 @@
 import openSocket from "socket.io-client";
-//export const SERVER_URL = "https://chess-server-z0of.onrender.com"
-export const SERVER_URL = "http://192.168.0.2:8080"
+export const SERVER_URL = "https://chess-server-z0of.onrender.com"
+//export const SERVER_URL = "http://192.168.0.2:8080"
 export const socket = openSocket(SERVER_URL)
