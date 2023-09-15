a485c5f146917f5785c9e18b2ac603d44c711c4a
Hamada Aljarrah <hamadasepost@gmail.com>
2023-09-15 14:48:52 +0200

    amogus

--- src/model/socket.ts
+++ src/model/socket.ts
@@ -3,5 +3,5 @@ import openSocket from "socket.io-client";
 const home = "http://192.168.0.2:8080";
 const school = "http://130.229.182.146:8080"
 const server = "https://chess-server-z0of.onrender.com";
-export const SERVER_URL = school;
+export const SERVER_URL = server;
 export const socket = openSocket(SERVER_URL)
